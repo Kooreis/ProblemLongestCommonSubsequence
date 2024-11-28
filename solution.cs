2e74ae8198329cadf64d68aa78733dcf2fa2646f
Kooreis <kooreis.official@gmail.com>
2024-11-28 06:44:46 -0600

    This part of the script fills the 2D array by comparing the characters of the input strings. If the current characters are the same, it increments the length of the longest common subsequence found so far. If the current characters are different, it takes the maximum length found so far.

--- solution.cs
+++ solution.cs
@@ -1,5 +1,10 @@
-function lcs(X, Y) {
-    let m = X.length;
-    let n = Y.length;
-    let L = Array(m + 1).fill().map(() => Array(n + 1).fill(0));
-}
+for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+            else if (X[i - 1] == Y[j - 1])
+                L[i][j] = L[i - 1][j - 1] + 1;
+            else
+                L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
+        }
+    }
